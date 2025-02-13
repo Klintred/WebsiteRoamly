73fdc0f74e95bec157366966d495473e1f75cf2c
milanaismail <133760937+milanaismail@users.noreply.github.com>
2025-02-13 14:30:02 +0100

    global colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
       </div>
       <ul className="nav-links">
         <li><Link to="/home">Home</Link></li>
-        <li><Link to="/trip-planner">AI Trip Planner</Link></li>
         <li><Link to="/my-trips">My Trips</Link></li>
-        <li><Link to="/reviews">Reviews</Link></li>
+        <li><Link to="/points">My points</Link></li>
         <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/trip-planner">Create a trip</Link></li>
       </ul>
     </nav>
   );
