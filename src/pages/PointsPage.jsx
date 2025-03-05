4c7a0b57cca4da7e8cbb7490d76c8bc5df7891c7
milanaismail <133760937+milanaismail@users.noreply.github.com>
2025-03-05 14:18:52 +0100

    homepage aanpassingen

--- src/pages/PointsPage.jsx
+++ src/pages/PointsPage.jsx
@@ -31,39 +31,38 @@ const PointsPage = () => {
                 />
             </div>
         </div>
-
-        <div className='badge-card'>
-            <div className='badge-card-header'>
-                <h2>Earn a badge</h2>
+        <div className="badge-container">
+            <div className='badge-card'>
+                <div className='badge-card-header'>
+                    <h2>Earn a badge</h2>
+                </div>
+                <div className='badge-card-content'>
+                    <p>
+                    By participating, you earn points; with these points, you can earn a verified badge.
+                    </p>
+                    <PrimaryButton
+                    text="How to earn points" 
+                    to="/how-to-earn-points" 
+                    variant="primary" 
+                    onClick={() => setMenuOpen(false)} 
+                    />
+                </div>
             </div>
-            <div className='badge-card-content'>
-                <p>
-                By participating, you earn points; with these points, you can earn a verified badge.
-                </p>
-                <PrimaryButton
-                text="How to earn points" 
-                to="/how-to-earn-points" 
-                variant="primary" 
-                onClick={() => setMenuOpen(false)} 
-                />
-            </div>
-        </div>
-
-{/*moet dynamisch zijn */}
-        <div className='badge-card'>
-            <div className='badge-card-header'>
-                <h2>Grand Plaza </h2> 
-            </div>
-            <div className='badge-card-content'>
-                <p>
-                    You recently went to the Grand Plaza. Earn points by writing a detailed review. 
-                </p>
-                <PrimaryButton
-                text="How to earn points" 
-                to="/how-to-earn-points" 
-                variant="primary" 
-                onClick={() => setMenuOpen(false)} 
-                />
+            <div className='badge-card'>
+                <div className='badge-card-header'>
+                    <h2>Grand Plaza </h2> 
+                </div>
+                <div className='badge-card-content'>
+                    <p>
+                        You recently went to the Grand Plaza. Earn points by writing a detailed review. 
+                    </p>
+                    <PrimaryButton
+                    text="How to earn points" 
+                    to="/how-to-earn-points" 
+                    variant="primary" 
+                    onClick={() => setMenuOpen(false)} 
+                    />
+                </div>
             </div>
         </div>
     </div>
