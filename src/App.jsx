a0c21a9ab6a7a0acd7d816c99bce739cfafab453
milanaismail <milana_is@hotmail.com>
2025-04-15 14:56:59 +0200

    hotjar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import AppRouter from './Router';
 import './styles/global.css';
+import Hotjar from '@hotjar/browser';
+
+const siteId = 6372598;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 function App() {
   return (
