8d8bb227ab15cadcc030541dc75fcef170c588a1
milanaismail <milana_is@hotmail.com>
2025-04-17 16:08:56 +0200

    vwo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppRouter from './Router';
 import './styles/global.css';
 import Hotjar from '@hotjar/browser';
+import { VWOScript } from 'vwo-smartcode-nextjs';
 
 const siteId = 6372598;
 const hotjarVersion = 6;
@@ -10,9 +11,16 @@ Hotjar.init(siteId, hotjarVersion);
 
 function App() {
   return (
-    <div className="App">
-      <AppRouter />
-    </div>
+    <html lang="en">
+    <head>
+      <VWOScript accountId="1072509" />
+    </head>
+    <body>
+      <div className="App">
+        <AppRouter />
+      </div>
+    </body>
+  </html>
   );
 
 }
