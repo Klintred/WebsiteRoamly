ca2681088bb14270c66892507437b0e2580012a9
Dante <r0841336@student.thomasmore.be>
2025-05-22 10:51:23 +0200

    change: api key

--- src/pages/HotelDetailPage.jsx
+++ src/pages/HotelDetailPage.jsx
@@ -109,7 +109,8 @@ const PlaceDetailPage = () => {
             height="300"
             frameBorder="0"
             style={{ border: 0, borderRadius: "8px" }}
-            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyARMMWTVxjvo8qABcvXgZpHt6FJL63CDpA&q=${coordinates}`}
+            //src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyARMMWTVxjvo8qABcvXgZpHt6FJL63CDpA&q=${coordinates}`}
+                        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBlpxT86DXT-8ugulNwJke4Oncf7yu7UcQ&q=${coordinates}`}
             allowFullScreen
           />
         </div>
