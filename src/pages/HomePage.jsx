8032408d383f2a1dc57917a804915d24954092bc
Thibault Rondelez <thibault.rondelez@outlook.com>
2025-06-06 15:43:07 +0200

    modal popup fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import '../styles/homepage.css';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import Popup from '../components/cards/Popup'; // ✅ NEW
+import Popup from '../components/cards/Popup'; 
 
 const API_BASE_URL = 'https://roamly-api.onrender.com';
 
